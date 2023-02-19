98d77ae1ffc58567e5f278bbee5476b31c510443
Shakilhasan347 <hasanshakil175694@gmail.com>
2023-02-19 19:42:19 +0600

    map method

--- src/App.js
+++ src/App.js
@@ -1,50 +1,23 @@
 import Card from "./Card";
 import Sdata from './Components/Sdata'
 import './index.css';
+let x;
+function item(val){
+ return (
+  <Card 
+    cardimg={val.cardimg}
+    heading={val.heading}
+    name={val.name}
+    link={val.link} />
+ );
+
+}
 
 function App() {
   return (
     <div className="App">
-   
-      <Card 
-      cardimg={Sdata[0].cardimg}
-      heading={Sdata[0].heading}
-      name={Sdata[0].name}
-      link={Sdata[0].link}
-      />
-       <Card 
-       cardimg={Sdata[1].cardimg}
-      heading={Sdata[1].heading}
-      name={Sdata[1].name}
-      link={Sdata[1].link}
-      />
-       <Card 
-        cardimg={Sdata[2].cardimg}
-        heading={Sdata[2].heading}
-        name={Sdata[2].name}
-        link={Sdata[2].link}
-      />
-       <Card 
-           cardimg={Sdata[3].cardimg}
-        heading={Sdata[3].heading}
-        name={Sdata[3].name}
-        link={Sdata[3].link}
-     
-
-      />
-        <Card 
-     cardimg={Sdata[4].cardimg}
-        heading={Sdata[4].heading}
-        name={Sdata[4].name}
-        link={Sdata[5].link}
-      />
-        <Card 
-       cardimg={Sdata[5].cardimg}
-        heading={Sdata[5].heading}
-        name={Sdata[5].name}
-        link={Sdata[5].link}
-      />
-    </div>
+    {Sdata.map(item)};
+      </div>
   );
 }
 export default App;
--- src/Components/Sdata.js
+++ src/Components/Sdata.js
@@ -1,21 +1,21 @@
 const Sdata=[
    {
       cardimg:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS15WXzJnh5FKZcjV7jwxj57HtPiu-mJyMBPJL7E2rPEFbLRPjA",
-      heading:"Original Netflix Serien",
+      heading:"Original Netflix Series",
       name:"DARK",
       link:""
      
    },
 
    { cardimg:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSqm3jNg3aWa_SZxyy88HmsjI18rwtAz6pSD_xDAE_HvViVi-Qk",
-    heading:"Original Netflix Serien",
+    heading:"Original Netflix Series",
     name:"MAINFAST",
     link:""
         },
 
         {
       cardimg:"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQqMMBVEw9TzyR0wJvIdXW7UClRiWMzlM7aUCpSKPlGfdt8YKhZ",
-      heading:"Original Netflix Serien",
+      heading:"Original Netflix Series",
       name:"BLOOD",
       link:""
 
@@ -23,7 +23,7 @@ const Sdata=[
         },
         {
             cardimg:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTQOtj4pUXP9fzyUz5gVYkZt3_1Z1gCNsGEPmT5snx_Xxhp0UNo",
-            heading:"Original Netflix Serien",
+            heading:"Original Netflix Series",
             name:"VIKINGS",
             link:""
 
@@ -33,13 +33,13 @@ const Sdata=[
         {
             
       cardimg:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRrwocOprYNKvHvW1CX-7SJX-s3A1J34vmsctr1hb6NdDHPgAl6",
-      heading:"Original Netflix Serien",
+      heading:"Original Netflix Series",
       name:"SHADOW",
       link:"https://www.imdb.com/title/tt2403776/"
         },
         {
             cardimg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvFfOhqNNdfTfhKZGiBk-2v7t2vlMG4RMu3BRDRmg-nJ-A8m2o",
-            heading:"Original Netflix Serien",
+            heading:"Original Netflix Series",
             name:"RECIDENT ",
             link:"https://www.imdb.com/title/tt2403776/"
         }
